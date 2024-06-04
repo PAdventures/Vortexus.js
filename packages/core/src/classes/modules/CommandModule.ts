919e7254ed32cf95615a33e5645c8b1493e908dd
PAdventures <sdulyea@icloud.com>
2024-06-04 18:25:02 +0100

    Feat: New Command module utility methods

--- packages/core/src/classes/modules/CommandModule.ts
+++ packages/core/src/classes/modules/CommandModule.ts
@@ -1,7 +1,10 @@
 import { PermissionResolvable } from "discord.js";
-import { CommandType } from "../../types/constants.js";
+import { CommandType, ModuleType } from "../../types/constants.js";
 import { AnyCommandBuilder, AnyCommandExecuteFunction } from "../../types/structures.js";
 import { BaseModule, BaseModuleData } from "./BaseModule.js";
+import { SlashCommandModule } from "./SlashCommandModule.js";
+import { ContextMenuCommandModule } from './ContextMenuCommandModule.js';
+import { MessageCommandModule } from "./MessageCommandModule.js";
 
 export interface CommandModuleData extends BaseModuleData {
     command_type: CommandType;
@@ -13,10 +16,23 @@ export interface CommandModuleData extends BaseModuleData {
 }
 
 export abstract class CommandModule extends BaseModule implements CommandModuleData {
+    public readonly module_type: ModuleType.CommandModule = ModuleType.CommandModule;
     public abstract readonly command_type: CommandType;
     public abstract data: AnyCommandBuilder;
     public required_bot_permission?: PermissionResolvable;
     public required_member_permissions?: PermissionResolvable;
     public cooldown?: number;
     public abstract execute: AnyCommandExecuteFunction;
+
+    public isSlashCommandModule(): this is SlashCommandModule {
+        return this.command_type === CommandType.SlashCommand
+    }
+
+    public isContextMenuCommand(): this is ContextMenuCommandModule {
+        return this.command_type === CommandType.ContextMenuCommand
+    }
+
+    public isMessageCommand(): this is MessageCommandModule {
+        return this.command_type === CommandType.MessageCommand
+    }
 }
