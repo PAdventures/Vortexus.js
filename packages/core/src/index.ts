1f9ec38591e6c5432cf71984dcd1a2cd416bc5b9
PAdventures <sdulyea@icloud.com>
2024-06-04 18:26:30 +0100

    Feat: index.ts in src added to export everything properly

---
+++ packages/core/src/index.ts
@@ -0,0 +1,15 @@
+export * from "./types/constants.js";
+export * from "./types/structures.js";
+
+export * from "./classes/builders/MessageCommandBuilder.js";
+export * from "./classes/builders/MessageCommandOptionBuilders.js";
+
+export * from "./classes/managers/CacheManager.js";
+export * from "./classes/managers/CommandManager.js"
+
+export * from "./classes/modules/CommandModule.js";
+export * from "./classes/modules/ContextMenuCommandModule.js";
+export * from "./classes/modules/MessageCommandModule.js";
+export * from "./classes/modules/SlashCommandModule.js";
+
+export * from "./classes/structures/Cooldowns.js"
