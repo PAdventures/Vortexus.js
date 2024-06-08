271fb9b49a5ce54d91a86a4cc47ee892a910cd4f
PAdventures <sdulyea@icloud.com>
2024-06-08 06:41:53 +0100

    Feat & Fix: Added a find method & fixed CodeFactor `major` unsafe declaration merge

--- packages/core/src/classes/managers/ModuleManagers.ts
+++ packages/core/src/classes/managers/ModuleManagers.ts
@@ -1,16 +1,33 @@
-import { AnyVortexusModule } from "../../types/structures.js";
+import { AnyCommandModule, AnyVortexusModule, AnyVortexusModuleData } from "../../types/structures.js";
+import { ContextMenuCommandModule, ContextMenuCommandModuleData } from "../modules/ContextMenuCommandModule.js";
+import { MessageCommandModule, MessageCommandModuleData } from "../modules/MessageCommandModule.js";
+import { SlashCommandModule, SlashCommandModuleData } from "../modules/SlashCommandModule.js";
+import { VortexusClient } from "../structures/VortexusClient.js";
 import { CacheManager } from "./CacheManager.js";
 
-export interface ModuleManager extends CacheManager<AnyVortexusModule> {}
+export class ModuleManager extends CacheManager<AnyVortexusModule> {
+    constructor(readonly client: VortexusClient) {
+        super()
+    }
 
-export abstract class ModuleManager extends CacheManager<AnyVortexusModule>  {
     public cacheModules(modules?: AnyVortexusModule[]): void {
         for (const module of modules ?? this.cache.values()) {
             this._cache.set(module.id, module)
         }
     }
 
-    public toJSON() {
+    public find(id: string): AnyVortexusModule | undefined
+    public find(module: SlashCommandModuleData): SlashCommandModule | undefined
+    public find(module: ContextMenuCommandModuleData): ContextMenuCommandModule | undefined
+    public find(module: MessageCommandModuleData): MessageCommandModule | undefined
+    public find(resolvable: string | AnyVortexusModuleData): AnyVortexusModule | undefined {
+        if (typeof resolvable === "string") {
+            return this._cache.get(resolvable);
+        }
+        return this._cache.get(resolvable.id)
+    }
+
+    public toJSON(): AnyCommandModule[] {
         return this.cache.map(m => m)
     }
 }
