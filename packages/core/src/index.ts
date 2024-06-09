ec57f5f82d8683031e1d847dcb79ad863acb3465
PAdventures <sdulyea@icloud.com>
2024-06-09 19:11:43 +0100

    Feat: Exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,11 +5,14 @@ export * from "./classes/builders/MessageCommandBuilder.js";
 export * from "./classes/builders/MessageCommandOptionBuilders.js";
 
 export * from "./classes/managers/CacheManager.js";
-export * from "./classes/managers/CommandManager.js"
+export * from "./classes/managers/CommandManager.js";
+export * from "./classes/managers/CooldownManager.js"
+export * from "./classes/managers/ModuleManagers.js"
 
 export * from "./classes/modules/CommandModule.js";
 export * from "./classes/modules/ContextMenuCommandModule.js";
 export * from "./classes/modules/MessageCommandModule.js";
 export * from "./classes/modules/SlashCommandModule.js";
+export * from "./classes/modules/PreconditionModule.js"
 
 export * from "./classes/structures/Cooldowns.js"
