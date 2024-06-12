fd683e0b4448cd589fab038733234a4a0a64502d
PAdventures <sdulyea@icloud.com>
2024-06-12 10:07:10 +0100

    Feat: toJSON method for command module

--- packages/core/src/classes/modules/CommandModule.ts
+++ packages/core/src/classes/modules/CommandModule.ts
@@ -1,14 +1,15 @@
-import { PermissionResolvable } from "discord.js";
+import { PermissionResolvable, RESTPostAPIChatInputApplicationCommandsJSONBody, RESTPostAPIContextMenuApplicationCommandsJSONBody } from "discord.js";
 import { CommandType, ModuleType } from "../../types/constants.js";
-import { AnyCommandBuilder, AnyCommandExecuteFunction } from "../../types/structures.js";
+import { AnyCommandBuilder, AnyCommandExecuteFunction, AnyCommandModuleData } from "../../types/structures.js";
 import { BaseModule, BaseModuleData } from "./BaseModule.js";
 import { SlashCommandModule } from "./SlashCommandModule.js";
 import { ContextMenuCommandModule } from './ContextMenuCommandModule.js';
 import { MessageCommandModule } from "./MessageCommandModule.js";
+import { MessageCommandBuilderData } from "../builders/MessageCommandBuilder.js";
 
 export interface CommandModuleData extends BaseModuleData {
     command_type: CommandType;
-    data: AnyCommandBuilder;
+    data: AnyCommandBuilder
     required_bot_permission?: PermissionResolvable;
     required_member_permissions?: PermissionResolvable;
     cooldown?: number;
@@ -34,5 +35,17 @@ export abstract class CommandModule extends BaseModule implements CommandModuleD
 
     public isMessageCommand(): this is MessageCommandModule {
         return this.command_type === CommandType.MessageCommand
+    }
+
+    public toJSON(): Omit<AnyCommandModuleData, "command_type" | "data"> & { command_type: CommandType, data: RESTPostAPIChatInputApplicationCommandsJSONBody | RESTPostAPIContextMenuApplicationCommandsJSONBody | MessageCommandBuilderData } {
+        return {
+            ...super.toJSON(),
+            command_type: this.command_type,
+            data: this.data.toJSON(),
+            cooldown: this.cooldown,
+            required_bot_permission: this.required_bot_permission,
+            required_member_permissions: this.required_member_permissions,
+            execute: this.execute
+        }
     }
 }
